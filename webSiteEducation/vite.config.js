0803b05fa2e288077a75dee924fd56f008424844
carlosgs05 <daniel18.dg95@gmail.com>
2025-07-11 21:11:47 -0500

    base route

--- webSiteEducation/vite.config.js
+++ webSiteEducation/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/webSiteEducacion/', // Aquí colocas el nombre de tu repositorio
   plugins: [react()],
 })
